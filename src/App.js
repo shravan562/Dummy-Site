e83b198cdf9c121805bb9b6752c493139fe01843
Shravan Gupta <83549529+shravan562@users.noreply.github.com>
2023-11-28 12:44:46 +0530

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,20 @@
+import React from 'react';
+import Navbar from './components/Navbar';
+import Hero from './components/Hero';
+import Analytics from './components/Analytic';
+import Newsletter from './components/Newsletter';
+import Footer from './components/Footer';
+function App() {
+  return (
+    <div>
+      <Navbar />
+      <Hero />
+      <Analytics />
+      <Newsletter />
+      <Footer />
+     
+    </div>
+  );
+}
+
+export default App; 
---
+++ src/components/Analytic.jsx
@@ -0,0 +1,24 @@
+import React from 'react';
+import Laptop from '../assets/laptop.jpg';
+
+const Analytics = () => {
+  return (
+    <div className='w-full bg-white py-16 px-4'>
+      <div className='max-w-[1240px] mx-auto grid md:grid-cols-2'>
+        <img className='w-[500px] mx-auto my-4' src={Laptop} alt='/' />
+        <div className='flex flex-col justify-center'>
+          <p className='text-[#00df9a] font-bold '>DATA ANALYTICS DASHBOARD</p>
+          <h1 className='md:text-4xl sm:text-3xl text-2xl font-bold py-2'>Manage Data Analytics Centrally</h1>
+          <p>
+            Centralized data in business analytics involves consolidating information from across an organization into a single system for easier analysis.
+            “This aspect of business intelligence and analytics is important because it gives companies visibility into KPIs at a high level,” says Brandon John, BluWave’s Service Provider Relationship Manager.
+            The consolidation usually happens in something called a data warehouse.
+          </p>
+          <button className='bg-black text-[#00df9a] w-[200px] rounded-md font-medium my-6 mx-auto md:mx-0 py-3'>Get Started</button>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Analytics;
